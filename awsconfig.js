357bb5c8c1c32d5336dca536c77707514ea9cc76
Veerav Naidu <veeravn@gmail.com>
2024-02-21 10:13:06 -0600

    remove secrets

--- awsconfig.js
+++ awsconfig.js
@@ -1,6 +1,6 @@
 awsOptions = {
-    accessKeyId: 'AKIATQQEVDB2ERERJZYU',
-    secretKey: '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',
+    accessKeyId: 'xxxxxxxxxxxxxxxxxxxxxxxx',
+    secretKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
     region: 'us-east-1',
-    bucketName: 'hybrid-storage-bucket'
+    bucketName: 'xxxxxxxxxxxxxxxxxx'
 }
